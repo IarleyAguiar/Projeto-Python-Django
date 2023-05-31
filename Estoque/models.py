6651e002697b2ffd631738f72dd0cbe2a3013ff5
Iarley Aguiar <iarley0456@gmail.com>
2023-05-31 20:34:37 -0300

    Teste

--- Estoque/models.py
+++ Estoque/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from Estoque.views import calculopedidos
 
+
 class Arroz(models.Model):
     nome_arroz = models.CharField(default='Arroz', editable=False, max_length=255, verbose_name="Nome do produto")
     qtd_arroz = models.IntegerField(verbose_name="Quantidade em estoque:")
@@ -11,6 +12,7 @@ class Arroz(models.Model):
 
     def __str__(self):
         return self.nome_arroz
+
 
 class Feijao(models.Model):
     nome_feijao = models.CharField(max_length=20, default="Feijão", editable=False, verbose_name="Feijao")
@@ -23,6 +25,7 @@ class Feijao(models.Model):
     def __str__(self):
         return self.nome_feijao
 
+
 class Refri(models.Model):
     nome_refri = models.CharField(max_length=20, default="Refri", editable=False, verbose_name="Refrigerante")
     qtd_refri = models.IntegerField(verbose_name="Quantidade em estoque:")
@@ -33,6 +36,8 @@ class Refri(models.Model):
 
     def __str__(self):
         return self.nome_refri
+
+
 # Atualizar um valor direto de um objeto já existente
 # Arroz.objects.filter(nome_arroz="Arroz").update(preco_arroz=3.12)
 # Receber um valor de dentro de um objeto específico
